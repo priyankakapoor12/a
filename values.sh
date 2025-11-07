dafc9ad8cb2f727cc1322e5765b96fe7895fb87e
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-07 15:01:50 +0530

    by me

---
+++ values.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+MOVIES=("Pushpa" "Dhoni" "Sachin")
+echo "First Movie = ${MOVIES[0]}"
+echo "Second Movie = ${MOVIES[1]}"
+echo "Third Movie = ${MOVIES[2]}"
+echo "All Movies = ${MOVIES[@]}"
