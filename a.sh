4fb55e897b741b636c6200d494e6e81797cdb578
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-07 14:30:54 +0530

    by me

--- a.sh
+++ a.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-echo "Hello World"
+#!/bin /bash
+PERSON1=Priyanka
+PERSON2=Priya
+PERSON3=P
+
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1."
+echo "$PERSON3: How are you $PERSON1 and $PERSON2?"
